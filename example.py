4671bcd5c50e0177b9e4c370e76bd7375c7f2a23
murphytarra <68056855+murphytarra@users.noreply.github.com>
2021-09-04 12:46:36 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,22 +1,14 @@
 
-"""
-Example code for ECHO 
-
-3 Level System with all parameters set to initial values
-
-For further information, see pdf 'Dispersive Readout of Multilevel Systems'
-
-by Tara Murphy
-
-Completed during 6 Week Summer Internship at Quantum Motion Technologies, UCL
-"""
-
-#%reset
-from Dispersive_Readout_Package import ECHO_3x3 as dr
-from Dispersive_Readout_Package import ECHO_2x2 as ds
-
-Q = dr.ECHO_3x3()
+import ECHO 
+from ECHO import ECHO_3x3 as e3
+from ECHO import ECHO_2x2 as e2
+Q = e3.ECHO_3x3()
 Q.print_parameters()
 Q.run()
 Q.coher_sweep(B = 10, gmin = 0.25, gmax = 2, emin = -4, emax = 4)
 Q.plot_eigen()
+Q.freq_sweep()
+Q.coher_sweep()
+
+P = e2.ECHO_2x2()
+P.run()
