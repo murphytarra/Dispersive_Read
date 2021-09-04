92c0c28dcb7785bee99fac342fbcb0d807b4d8ee
murphytarra <68056855+murphytarra@users.noreply.github.com>
2021-09-04 11:58:18 +0100

    Update __init__.py

--- Dispersive_Readout_Package/__init__.py
+++ Dispersive_Readout_Package/__init__.py
@@ -1,5 +1,5 @@
-import Dispersive_Readout_Package
-from Dispersive_Readout_Package import Dispersive_Readout
+import ECHO
+from ECHO import ECHO_3x3
 #from Dispersive_Readout import Dispersive_Readout
 from Dispersive_Readout_Package import ECHO_2x2
 
